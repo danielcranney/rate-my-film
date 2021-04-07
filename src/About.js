67fb06ee0a78237728cf6156de1ebd6619a00a11
djcranney <danieljamescranney@gmail.com>
2021-04-07 10:52:14 +0100

    Minor change

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <main className="about-wrapper">
             <div className="about-section">
                     <div>
-                        <h3>About</h3>
+                        <h1>About</h1>
                         <p className="lead-in-text">Rate My Film was built in April 2021 by web developer and filmmaking teacher Daniel Cranney as a way to help student, amateur, no-budget or low-budget filmmakers learn more about how films are age rated in the UK, in a user-friendly and hassle-free way.</p>
                         <p className="faded-text">The <a href="https://www.bbfc.co.uk/" target="_blank" rel="noreferrer">British Board of Film Classification</a> (BBFC) are in charge of age rating films in the UK, and their website has plenty of information about what makes a film U-rated, or only suitable for over 18's (and everything in between).</p>
                         <p className="faded-text">Rate My Film is built using data made available by the BBFC (and some from IMDB, too), and while it can't give you an official rating, it can help you get a better idea of who your film might be suitable for, and why.</p>
