844c9e879450f360461b366344638f12ea1f595d
djcranney <danieljamescranney@gmail.com>
2021-04-13 16:59:52 +0100

    Removing errors for deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from './Header';
 import Footer from './Footer';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-import Home from './Home';
-import About from "./About";
+import {
+  // Route,
+  BrowserRouter as Router,
+  // Switch
+} from "react-router-dom";
+// import Home from './Home';
+// import About from "./About";
 
 const App = () => {
 
