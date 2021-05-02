c4bdba1d03d92f7a0a582b7fa1d073de5704de9c
djcranney <danieljamescranney@gmail.com>
2021-05-02 19:12:46 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         { showContent ? <Content /> : (
             <main className="main-wrapper landing-bg">
       
-              <div className="column landing-left">
+              {/* <div className="column landing-left">
                 <animated.h5 style={noDelay}>CALLING ALL FILMMAKERS!</animated.h5>
                 <animated.h1 style={noDelay}>Get to know your audience</animated.h1>
                 <animated.p style={mediumDelay}>Get a free age rating for your film in just a few clicks, using data provided by the British Board of Film Classification.</animated.p>
@@ -25,7 +25,7 @@ const Home = () => {
                   }
                   style={longDelay} 
                   className="start-button">Get Started</animated.button>
-              </div>
+              </div> */}
             </main>
           )}
           </>
